47b5127f0256e882ff755f8c1ef92730fdada4d2
albertodottofli-lang <albertodottofli@gmail.com>
2025-10-21 22:49:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
 const nextConfig = { reactStrictMode: true }; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: false, // <---- AGGIUNGI QUESTO
+  },
+};
+
+export default nextConfig;
